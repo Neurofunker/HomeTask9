88742a33ab4c4c5711573187b42ad83dceab5ec4
Makc <m_stefanenkov@mail.ru>
2023-02-26 14:19:42 +0100

    Решил задачу 66

--- Task66/Program.cs
+++ Task66/Program.cs
@@ -12,9 +12,7 @@ int number2 = Convert.ToInt32(Console.ReadLine());
 int SumNaturalNumbers(int num1, int num2)
 {
     if (num1 == num2 + 1) return 0;
-    else number1 += SumNaturalNumbers(number1 + 1, number2);
-    return SumNaturalNumbers(number1, number2);
-    
+    return num1 += SumNaturalNumbers(num1 + 1, num2);
 }
 if(number1 > 0 && number2 > 0) Console.WriteLine(SumNaturalNumbers(number1, number2));
 else Console.WriteLine("Число не натуральное!");
